de7217f1ed1a7418cbe4129ced86cfc31126a9b0
Alexei Kubasov <kubasovalexei@yandex.ru>
2021-06-17 12:25:09 +0700

    feat: различные состояния кнопки авторизации в header

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,10 +17,19 @@ function Header (props) {
                 <p 
                     className="header__auth-item header__auth-item_logout" 
                     onClick={() => props.onLogout()}>
-                        Logout
+                        Выйти
                 </p>
                 }            
-                
+                {loc.pathname==="/login" &&
+                <Link to="/register" className="header__auth-item header__auth-item_login">
+                        Регистрация
+                </Link>
+                }    
+                {loc.pathname==="/register" &&
+                <Link to="/login" className="header__auth-item header__auth-item_login">
+                        Войти
+                </Link>
+                }      
             </div>
         </header>
     )
