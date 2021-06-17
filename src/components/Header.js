ea28fb6c93d7a5e9c456fa45291e5d82128ba716
Alexei Kubasov <kubasovalexei@yandex.ru>
2021-06-17 12:05:20 +0700

    feat: определение текущего адреса страницы для разного состояния кнопки логин/разлогин/регистрация

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import logo from '../images/logo-mesto.svg';
 
 function Header (props) {
+
+    const loc = useLocation()
+
     return (
         <header className="header">
             <Link to="/"><img className="header__logo" src={logo} alt="Логотип сайта" /></Link>
             <div class="header__auth">
                 {props.isLoggedIn ? 
                 (<p className="header__auth-item header__auth-item_email">{localStorage.getItem('email')}</p>) : 
-                null}                
-                <p className="header__auth-item header__auth-item_logout" onClick={() => props.onLogout()}>Logout</p>
+                null} 
+                {loc.pathname==='/' &&
+                <p 
+                    className="header__auth-item header__auth-item_logout" 
+                    onClick={() => props.onLogout()}>
+                        Logout
+                </p>
+                }            
+                
             </div>
         </header>
     )
